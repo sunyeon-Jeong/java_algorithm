ee116d2747a6a433dfb868758a8925e94e91ccf1
mallang <sunyeon4012@gmail.com>
2023-06-20 11:28:14 +0900

    [unrated] Title: a와 b 출력하기, Time: 183.78 ms, Memory: 68.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181951. a와 b 출력하기/a와 b 출력하기.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Algorithm2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
