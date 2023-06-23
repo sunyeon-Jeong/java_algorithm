234c49e4e933f4100ab3dba75e17c9506b1ef52f
mallang <sunyeon4012@gmail.com>
2023-06-23 14:34:27 +0900

    [unrated] Title: 덧셈식 출력하기, Time: 167.39 ms, Memory: 67.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181947. 덧셈식 출력하기/덧셈식 출력하기.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Algorithm6 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        
+        System.out.println(a + " + " + b + " = " + (a + b));
+    }
+}
