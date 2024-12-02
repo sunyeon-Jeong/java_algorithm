4ac465a2b2332e71ec93ec3e78cd07b7fa8a69f4
mallang <sunyeon4012@gmail.com>
2024-12-02 21:45:48 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.02 ms, Memory: 80.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int[] array, int height) {
+        int answer = 0;
+        
+        for (int i : array) {
+            if (i > height) {
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
