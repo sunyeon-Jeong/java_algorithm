98b72cbbfc4f924e89aac60f5710951a21ace144
mallang <sunyeon4012@gmail.com>
2024-12-21 11:44:38 +0900

    [level 0] Title: 접미사 배열, Time: 0.41 ms, Memory: 84 MB -BaekjoonHub

---
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String my_string) {
+        String[] answer = new String[my_string.length()];
+        
+        for (int i = 0; i < my_string.length(); i++) {
+            answer[i] = my_string.substring(i);
+        }
+        
+        // 정렬
+        Arrays.sort(answer);
+        
+        return answer;
+    }
+}
