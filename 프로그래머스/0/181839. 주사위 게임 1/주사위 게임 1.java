0134754a35bea23a1e9a3919178a8c10d3b05270
mallang <sunyeon4012@gmail.com>
2024-12-21 17:30:50 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.02 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int a, int b) {
+        int answer = 0;
+        
+        if (a % 2 != 0 && b % 2 != 0) {
+            answer = a*a + b*b;
+        } else if (a % 2 != 0 || b % 2 != 0) {
+            answer = 2 * (a+b);
+        } else if (a % 2 == 0 && b % 2 == 0) {
+            answer = a - b;
+            if (answer < 0) {
+                answer *= -1;
+            }
+        }
+        
+        return answer;
+    }
+}
